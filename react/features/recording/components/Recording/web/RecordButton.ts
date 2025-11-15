f975c60c650765b2ed5ebf8e5604d4c973a545f1
sandunsameera <sandunsameera25@gmail.com>
2025-11-15 16:55:09 +0530

    feat: add autopin-toggle to constants for toolbar and participants pane

--- react/features/recording/components/Recording/web/RecordButton.ts
+++ react/features/recording/components/Recording/web/RecordButton.ts
@@ -78,9 +78,14 @@ class RecordingButton extends AbstractRecordButton<IProps> {
      * @returns {void}
      */
     _startRecording() {
-        const { _conference } = this.props;
+        const { _conference, dispatch } = this.props;
 
         if (_conference) {
+            // Enable tile view before starting recording to ensure correct layout
+            import('../../../../video-layout/actions.web').then(({ setTileView }) => {
+                dispatch(setTileView(true));
+            });
+
             // Start Jibri recording with file recording metadata
             const appData = JSON.stringify({
                 'file_recording_metadata': {
