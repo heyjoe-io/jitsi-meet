392d891a874c136918817d67e414a8e9dac0696d
sandunsameera <sandunsameera25@gmail.com>
2025-11-19 00:01:13 +0530

    feat: add autopin-toggle to constants for toolbar and participants pane

---
+++ react/features/toolbox/components/web/PinAllCamerasButton.tsx
@@ -0,0 +1,98 @@
+import { connect } from 'react-redux';
+
+import { createToolbarEvent } from '../../../analytics/AnalyticsEvents';
+import { sendAnalytics } from '../../../analytics/functions';
+import { IReduxState } from '../../../app/types';
+import { translate } from '../../../base/i18n/functions';
+import { IconPin } from '../../../base/icons/svg';
+import { MEDIA_TYPE, VIDEO_TYPE } from '../../../base/media/constants';
+import { pinParticipant } from '../../../base/participants/actions';
+import { getRemoteParticipants } from '../../../base/participants/functions';
+import AbstractButton, { IProps as AbstractButtonProps } from '../../../base/toolbox/components/AbstractButton';
+
+/**
+ * The type of the React {@code Component} props of {@link PinAllCamerasButton}.
+ */
+interface IProps extends AbstractButtonProps {
+
+    /**
+     * Redux dispatch function.
+     */
+    dispatch: Function;
+
+    /**
+     * Array of participant IDs with camera enabled.
+     */
+    participantsWithCamera: string[];
+}
+
+/**
+ * Implementation of a button for pinning all participants with camera enabled.
+ */
+class PinAllCamerasButton extends AbstractButton<IProps> {
+    override accessibilityLabel = 'toolbar.accessibilityLabel.pinAllCameras';
+    override icon = IconPin;
+    override label = 'toolbar.pinAllCameras';
+    override tooltip = 'toolbar.pinAllCameras';
+
+    /**
+     * Handles clicking the button to pin all participants with camera enabled.
+     *
+     * @private
+     * @returns {void}
+     */
+    override _handleClick() {
+        const { dispatch, participantsWithCamera } = this.props;
+
+        sendAnalytics(createToolbarEvent('pin.all.cameras'));
+
+        participantsWithCamera.forEach(participantId => {
+            dispatch(pinParticipant(participantId));
+        });
+    }
+
+    /**
+     * Indicates whether this button is disabled or not.
+     *
+     * @override
+     * @protected
+     * @returns {boolean}
+     */
+    override _isDisabled() {
+        return this.props.participantsWithCamera.length === 0;
+    }
+}
+
+/**
+ * Function that maps parts of Redux state tree into component props.
+ *
+ * @param {Object} state - Redux state.
+ * @returns {Object}
+ */
+function mapStateToProps(state: IReduxState) {
+    const remoteParticipants = getRemoteParticipants(state);
+    const participantsWithCamera: string[] = [];
+
+    remoteParticipants.forEach((participant, id) => {
+        if (participant.sources) {
+            const videoSources = participant.sources.get(MEDIA_TYPE.VIDEO);
+
+            if (videoSources) {
+                const hasCameraEnabled = Array.from(videoSources.values()).some(
+                    source => source.videoType === VIDEO_TYPE.CAMERA && !source.muted
+                );
+
+                if (hasCameraEnabled) {
+                    participantsWithCamera.push(id);
+                }
+            }
+        }
+    });
+
+    return {
+        participantsWithCamera,
+        visible: participantsWithCamera.length > 0
+    };
+}
+
+export default translate(connect(mapStateToProps)(PinAllCamerasButton));
